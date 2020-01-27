edd73205457dac102792889899c373cc709a2c1f
mizelan <mizelan@pc>
2020-01-27 18:42:14 +0900

    spring boot 프로젝트 추가

---
+++ backend/src/test/kotlin/com/mizelan/kotlinBoard/KotlinBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mizelan.kotlinBoard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
